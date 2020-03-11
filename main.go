e6520ef6d72c9fb144916b29ada6903ccee38cbc
blademainer <blademainer@gmail.com>
2020-03-11 15:02:40 +0800

    add gin

--- main.go
+++ main.go
@@ -1,17 +1,28 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		_, err := writer.Write([]byte("hello!\n"))
-		if err != nil {
-			fmt.Printf("write error: %v \n", err.Error())
-		}
-	})
-	err := http.ListenAndServe(":8080", nil)
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	}).
+		GET("/", func(c *gin.Context) {
+			c.Writer.Write([]byte("hello!\n"))
+			//c.String(http.StatusOK, "", "hello!\n")
+		})
+	err := r.Run(":8081")
+
+	//http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+	//	_, err := writer.Write([]byte("hello!\n"))
+	//	if err != nil {
+	//		fmt.Printf("write error: %v \n", err.Error())
+	//	}
+	//})
+	//err := http.ListenAndServe(":8081", nil)
 	panic(err)
 }
