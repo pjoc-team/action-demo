74d379c4a5d26db5cd9c2a0497f920ecf80c6796
blademainer <blademainer@gmail.com>
2020-03-12 12:17:32 +0800

    remove cache

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 repository=`cat go.mod | grep -E "^module\s[0-9a-zA-Z\./_\-]+" | awk '{print $2}'`
-docker build --build-arg repository=$repository -v /go/pkg/mod:/go/pkg/mod . -t image --file Dockerfile
+docker build --build-arg repository=$repository . -t image --file Dockerfile
