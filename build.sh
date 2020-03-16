abf45ee42a83be4db0485acce4d9b0c93f78eaab
blademainer <blademainer@gmail.com>
2020-03-17 00:32:57 +0800

    update

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
-repository=`cat go.mod | grep -E "^module\s[0-9a-zA-Z\./_\-]+" | awk '{print $2}'`
-docker build --build-arg repository=$repository . -t image --file Dockerfile
+export REPOSITORY=`cat go.mod | grep -E "^module\s[0-9a-zA-Z\./_\-]+" | awk '{print $2}'`
+export NAME=`basename $REPOSITORY`
+docker build --build-arg repository=$REPOSITORY . -t image --file Dockerfile
